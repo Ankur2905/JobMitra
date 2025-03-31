42b89b94f46ad5f0ad1a337b50460b917810c7a8
Ankur <singhankur362@gmail.com>
2025-03-31 17:18:28 +0530

    Initial Job API Implementation: Get All Jobs, Create Job, Get Single Job

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ dotenv.config();
 import express from "express";
 const app = express();
 import morgan from "morgan";
+import { nanoid } from "nanoid";
+
+let jobs = [
+  { id: nanoid(), company: "Amazon", position: "SDE" },
+  { id: nanoid(), company: "Google", position: "Frontend Developer" },
+];
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -17,6 +23,33 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", data: req.body });
+});
+
+// Get All Jobs
+app.get("/api/jobs", (req, res) => {
+  res.status(200).json({ jobs });
+});
+
+// Create Job
+app.post("/api/jobs", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "Please provide all details" });
+  }
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(201).json({ job });
+});
+
+// Get Single Job
+app.get("/api/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with ID ${id}` });
+  }
+  res.status(200).json({ job });
 });
 
 const port = process.env.PORT || 5100;
