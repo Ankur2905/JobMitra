b767b8e850453aa62063d9d493dd282efc551693
Ankur <singhankur362@gmail.com>
2025-04-07 17:20:39 +0530

    Completed All jobs page

--- client/src/components/FormRow.jsx
+++ client/src/components/FormRow.jsx
@@ -1,4 +1,4 @@
-const FormRow = ({ type, name, labelText, defaultValue }) => {
+const FormRow = ({ type, name, labelText, defaultValue, onChange }) => {
   return (
     <div className="form-row">
       <label htmlFor={name} className="form-label">
@@ -8,8 +8,9 @@ const FormRow = ({ type, name, labelText, defaultValue }) => {
         type={type}
         id={name}
         name={name}
-        defaultValue={defaultValue}
+        defaultValue={defaultValue || ''}
         className="form-input"
+        onChange={onChange}
         required
       />
     </div>
